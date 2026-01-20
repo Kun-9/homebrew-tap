96fe3c2465c72718729c1c1726ebdc4ca2ece0de
kun <sus5330@icloud.com>
2026-01-21 00:41:55 +0900

    [PUBLISH] 1.0.2

--- Casks/claude-session-manager.rb
+++ Casks/claude-session-manager.rb
@@ -1,6 +1,6 @@
 cask "claude-session-manager" do
-  version "1.0.0"
-  sha256 "629bbfba83b96b48df5557732c7e7262e93942daf13eebdb3fe92ddf02bd758b"
+  version "1.0.2"
+  sha256 "807e783fb5c25f08f087a37637cdee549b66a8b382480fc1b5dd9e02bc157545"
 
   url "https://github.com/Kun-9/CLAUDE-SESSION-MANAGER/releases/download/v#{version}/ClaudeSessionManager-#{version}.dmg"
   name "Claude Session Manager"
